cd8b336a95a0347fe8de65ec3183ef72dd80768a
Felipe Larson da Silveira <felipe.silveira@enline-transmission.com>
2023-07-04 11:55:54 +0100

    feat: version 04-07-23 11:55

--- src/components/Learn/CardVideo/cardVideoData.ts
+++ src/components/Learn/CardVideo/cardVideoData.ts
@@ -1,39 +1,44 @@
 export const CARD_VIDEO_DATA = [
 	{
 		id: '1',
-		heading: 'Getting Started with Enline',
-		description: `This tutorial will walk you through the basics of setting up your account, understanding the platform's features, and implementing the technology to transform your energy infrastructure.`,
-		videoUrl: 'https://www.youtube.com/embed/2QunmjDyp-4'
+		heading: "Efficient Energy Monitoring with Enline's Cockpit Feature",
+		description:
+			"Discover Enline's Cockpit feature, designed to provide real-time monitoring and analysis of your energy infrastructure. Learn how to select transmission lines, set time filters, and explore key performance indicators (KPIs) for optimal energy management. Maximize your usage with Enline's intuitive interface and take control of your energy systems.",
+		videoUrl: 'https://www.youtube.com/embed/UjgkYccvToQ'
 	},
 	{
 		id: '2',
-		heading: 'Enline LiveView: A Comprehensive Walkthrough',
-		description: `This comprehensive walkthrough will guide you through the platform's features, showing you how to monitor your energy assets effectively, analyze data, and make informed decisions to boost efficiency and performance.`,
-		videoUrl: 'https://www.youtube.com/embed/svYiy6aFqlo'
-	},
-	{
-		id: '3',
-		heading: 'Mastering Enline PredictPro: Tips & Tricks',
-		description: `Learn how to harness the power of AI-driven predictive analytics to forecast potential issues, optimize your energy infrastructure, and stay one step ahead of potential threats.`,
+		heading: "Uncover Insights with Enline's Data Analysis Feature",
+		description:
+			"Dive into Enline's Data Analysis feature, allowing you to analyze and visualize energy data with ease. Explore time-series plotting, distance-based representations, and 3D curves to gain comprehensive insights. Learn how to leverage statistical KPIs and charts to make data-driven decisions for energy optimization. Empower your energy management with Enline's advanced analytics capabilities.",
 		videoUrl: 'https://www.youtube.com/embed/MsuhWToag44'
 	},
 	{
+		id: '3',
+		heading: "In-Depth Analysis with Enline's Statistics Feature",
+		description:
+			"Unlock the power of data analysis with Enline's Statistics feature. Discover maximum, minimum, average, and standard deviation values for your selected variables. Explore cumulative probability plots, probability distributions, and histograms to understand data patterns. Leverage the main chart for interactive exploration of data. Empower your decision-making with Enline's statistical insights.",
+		videoUrl: 'https://www.youtube.com/embed/svYiy6aFqlo'
+	},
+	{
 		id: '4',
-		heading: 'Ensuring Safety with Enline SafeGuard',
-		description: `This tutorial will demonstrate how to leverage advanced algorithms and data-driven insights to optimize your energy infrastructure, minimize losses, and reduce operational costs for peak performance.`,
+		heading: "Stay Informed with Enline's Alerts List Feature",
+		description:
+			"Enline's Alerts List feature keeps you on top of your energy infrastructure. Learn how to navigate through predictive and historical alerts, view alert details, and analyze affected regions. Upload relevant documents and classify alerts for efficient management. Communicate with your team using the comments sidebar. Ensure proactive monitoring and quick responses with Enline's Alerts List.",
 		videoUrl: 'https://www.youtube.com/embed/SBIrTqaIXto'
 	},
 	{
 		id: '5',
-		heading: 'Peak Performance with Enline OptiMax',
-		description: `1.This tutorial will explain how our solution helps you identify and mitigate potential risks, ensuring the safety of your assets and the continuity of your operations.
-    2. Designing Efficient Projects with Enline SmartDesign`,
+		heading: "Customize Your Experience with Enline's User Settings",
+		description:
+			"Enline's User Settings feature allows you to personalize your Enline profile and optimize your experience. Update your information, adjust time zones, and enhance security with password updates. Customize your notification preferences to suit your needs. Tailor your Enline experience and ensure seamless energy infrastructure management with user-friendly settings.",
 		videoUrl: 'https://www.youtube.com/embed/6ERiWcJKfoI'
 	},
 	{
 		id: '6',
-		heading: 'Designing  Projects with Enline SmartDesign',
-		description: `This tutorial will introduce you to the innovative features of our design tool, guiding you through the process of creating energy-efficient projects tailored to your specific needs and objectives.`,
-		videoUrl: 'https://www.youtube.com/embed/UjgkYccvToQ'
+		heading: "Real-Time Insights with Enline's Digital Twin Feature",
+		description:
+			"Explore Enline's Digital Twin feature, your window into real-time updates of your energy assets. Get an overview of your digital twin, including alert triggering areas. Interact with the 3D view to explore alert details and understand their impact. Customize the display of alert types using the variable toggle. Leverage the power of Enline's Digital Twin to optimize your energy infrastructure management.",
+		videoUrl: 'https://www.youtube.com/embed/2QunmjDyp-4'
 	}
 ];
